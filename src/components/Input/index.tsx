c53dfdc87c35892a84b329a7a8906873353e4fd5
leandro moraes <leandromoraes12345@outlook.com>
2020-10-20 07:09:58 -0300

    finish

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
@@ -9,10 +10,22 @@ interface PropsInput extends InputHTMLAttributes<HTMLInputElement>{
 };
 
 const Input: React.FC<PropsInput> = ({ name, icon: Icon, ...res }) => {
-  return (
+    const inputRef = useRef<HTMLInputElement>(null);
+    const { fieldName, defaultValue, error, registerField } = useField(name);
+    
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            ref: inputRef.current,
+            path: 'value'
+        })
+    }, [fieldName, registerField]);
+
+    return (
       <Container>
           {Icon && <Icon size={20} />}
           <input 
+          ref={inputRef}
           name={name} 
           {...res} />
       </Container>
--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FaClipboardList, FaUserCircle, FaAddressCard , FaMailchimp} from 'react-icons/fa';
+import { Form } from '@unform/web';
+import * as yup from 'yup';
 
 import logoImg from '../../assets/logo.png';
 
@@ -8,26 +10,41 @@ import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const Form: React.FC = () => {
+const PageForm: React.FC = () => {
+
+  const handleSubmit = (data: object) => {
+
+    const schema = yup.object().shape({
+      name: yup.string().required('Nome é obrigatório'),
+      address: yup.string().required('Nome é obrigatório'),
+      address2: yup.string().required('Nome é obrigatório'),
+      city: yup.string().required('Nome é obrigatório'),
+      province: yup.string().required('Nome é obrigatório'),
+      email: yup.string().email('E-mail inválido').required('Nome é obrigatório')
+    });
+
+    console.log(data);
+  };
+
   return (
       <Container>
           <Content>
             <img src={logoImg} />
 
-            <form>
+            <Form onSubmit={handleSubmit} >
                 <h1>Preencha o formulario e fale conosco</h1>
             
-                <Input name="firstName" icon={FaUserCircle}  placeholder="Primeiro nome" />
-                <Input name="lastName" icon={FaUserCircle} placeholder="Último nome" />
-                <Input name="lastName" icon={FaAddressCard} placeholder="Endereço" />
-                <Input name="lastName" icon={FaAddressCard} placeholder="Unidade / Apto" />
-                <Input name="lastName" icon={FaAddressCard} placeholder="Cidade" />
-                <Input name="lastName" icon={FaAddressCard} placeholder="Província / Território" />
-                <Input name="email" icon={FaMailchimp} placeholder="E-mail" />
+                <Input name="name" maxLength={80} icon={FaUserCircle}  placeholder="Nome"/>
+                <Input name="address"  maxLength={128} icon={FaAddressCard} placeholder="Endereço" />
+                <Input name="address2" maxLength={128} icon={FaAddressCard} placeholder="Unidade / Apto" />
+                <Input name="city"  maxLength={32} icon={FaAddressCard} placeholder="Cidade" />
+                <Input name="province"  maxLength={32} icon={FaAddressCard} placeholder="Província / Território" />
+                <Input name="email" maxLength={128} icon={FaMailchimp} placeholder="E-mail" />
             
                 <Button type='submit'>Enviar</Button>
 
-            </form>
+            </Form>
+
             <a href="#">Ver Lista de pedidos <FaClipboardList /></a>
 
           </Content>
@@ -36,4 +53,4 @@ const Form: React.FC = () => {
   );
 }
 
-export default Form;
+export default PageForm;
